fdd5b570d94954e18cd7f312c8b71cf88148b4d0
Bruno Chapdelaine <bruno.chapdelaine@usherbrooke.ca>
2019-05-23 18:20:19 -0400

    Update EPR2_RollController.cpp

--- libraries/APM_Control/EPR2_RollController.cpp
+++ libraries/APM_Control/EPR2_RollController.cpp
@@ -13,8 +13,7 @@
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-//	Code by Jon Challinger
-//  Modified by Paul Riseborough
+//	Code by Bruno Chapdelaine
 //
 
 #include <AP_HAL/AP_HAL.h>
