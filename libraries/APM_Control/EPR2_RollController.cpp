25a5f6cffd2e763ce44620fbe2edfc403400866e
chapdbr <bruno.chapdelaine@usherbrooke.ca>
2019-05-28 09:25:02 -0400

    correction

--- libraries/APM_Control/EPR2_RollController.cpp
+++ libraries/APM_Control/EPR2_RollController.cpp
@@ -128,7 +128,7 @@ int32_t EPR2_RollController::get_servo_out(float desired_angle)
 	// Multiply roll error by gains.I and integrate
 
 	// Don't integrate if in stabilise mode as the integrator will wind up against the pilots inputs
-	if (!disable_integrator && _ki > 0) {
+	if (_ki > 0) {
 		//only integrate if gain and time step are positive and airspeed above min value.
 		if (dt > 0 && aspeed > float(aparm.airspeed_min)) {
 		    float integrator_delta = angle_error * _ki * delta_time;
